5b7239b3b645e2389d987dc4a3833915445233c8
Tao Hu <taohu620@gmail.com>
2018-06-25 01:34:36 +0800

    u

--- example/incremental-seg/datalist/prepare.py
+++ example/incremental-seg/datalist/prepare.py
@@ -363,6 +363,9 @@ def total19(label_img):
 
     return True, label_img
 
+
+def coco2voc
+
 """
 conduct_filter(filter_func=filter10_old, result_dir ="class10+10/old", label_dir ="/home/hutao/dataset/incremental_seg/class10+10_old")
 conduct_filter(filter_func=filter10_new, result_dir ="class10+10/new", label_dir ="/home/hutao/dataset/incremental_seg/class10+10_new")
@@ -393,11 +396,18 @@ conduct_filter(filter_func=single17, result_dir ="single17", label_dir ="/home/h
 conduct_filter(filter_func=single18, result_dir ="single18", label_dir ="/home/hutao/dataset/incremental_seg/single18")
 conduct_filter(filter_func=single19, result_dir ="single19", label_dir ="/home/hutao/dataset/incremental_seg/single19")
 conduct_filter(filter_func=single20, result_dir ="single20", label_dir ="/home/hutao/dataset/incremental_seg/single20")
-"""
 
 conduct_filter(filter_func=total16, result_dir ="total16", label_dir ="/home/hutao/dataset/incremental_seg/total16")
 conduct_filter(filter_func=total17, result_dir ="total17", label_dir ="/home/hutao/dataset/incremental_seg/total17")
 conduct_filter(filter_func=total18, result_dir ="total18", label_dir ="/home/hutao/dataset/incremental_seg/total18")
 conduct_filter(filter_func=total19, result_dir ="total19", label_dir ="/home/hutao/dataset/incremental_seg/total19")
+"""
+
+
+
+conduct_filter(filter_func=filter15_gradual_new20, result_dir ="class15_gradual/new20", label_dir ="/home/hutao/dataset/incremental_seg/class15_gradual_new20")
+
+
+
 
 
