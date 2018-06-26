be84fbb7cee8ed96cd95a9e184d685a6e4ed64c8
Tao Hu <taohu620@gmail.com>
2018-06-26 16:32:35 +0800

    u

--- example/few-shot-det/ss_datalayer.py
+++ example/few-shot-det/ss_datalayer.py
@@ -2,13 +2,200 @@
 import numpy as np
 import random,os
 from multiprocessing import Process, Queue, Pool, Lock
-
+import os.path as osp
 import sys
 import traceback
 import util
 from util import cprint, bcolors
 from skimage.transform import resize
-import copy
+import copy,cv2,pickle
+
+class PASCAL_READ_MODES:
+    #Returns list of DBImageItem each has the image and one object instance in the mask
+    INSTANCE = 0
+    #Returns list of DBImageItem each has the image and the mask for all semantic labels
+    SEMANTIC_ALL = 1
+    #Returns list of DBImageSetItem each has set of images and corresponding masks for each semantic label
+    SEMANTIC = 2
+
+
+class DBImageSetItem():
+    def __init__(self, name, image_items=[]):
+        self.name = name
+        self.length = len(image_items)
+        self.image_items = image_items
+
+    def append(self, image_item):
+        self.image_items.append(image_item)
+        self.length += 1
+
+    def read_img(self, img_id):
+        return self.image_items[img_id].read_img()
+
+    def read_mask(self, img_id):
+        return self.image_items[img_id].read_mask()
+
+
+class DBPascalItem():
+    def __init__(self, name, img_path, mask_path, obj_ids, ids_map=None):
+        self.name = name
+        self.img_path = img_path
+        self.mask_path = mask_path
+        self.obj_ids = obj_ids
+        if ids_map is None:
+            self.ids_map = dict(zip(obj_ids, np.ones(len(obj_ids))))
+        else:
+            self.ids_map = ids_map
+
+    def read_mask(self, orig_mask=False):
+        mobj_uint = cv2.imread(self.mask_path, cv2.IMREAD_GRAYSCALE)
+        if orig_mask:
+            return mobj_uint.astype(np.float32)
+        m = np.zeros(mobj_uint.shape, dtype=np.float32)
+        for obj_id in self.obj_ids:
+            m[mobj_uint == obj_id] = self.ids_map[obj_id]
+        return m
+
+    def read_img(self):
+        #return read_img(self.img_path), simple
+        pass
+
+
+
+
+class PASCAL:
+    def __init__(self, db_path, dataType):
+
+        assert dataType == "train" or dataType == "val"
+        self.db_path = db_path
+        classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
+                   'dog', 'horse', 'motorbike', 'person', 'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor']
+        self.name_id_map = dict(zip(classes, range(1, len(classes) + 1)))
+        self.id_name_map = dict(zip(range(1, len(classes) + 1), classes))
+        self.dataType = dataType
+
+    def getCatIds(self, catNms=[]):
+        return [self.name_id_map[catNm] for catNm in catNms]
+
+    def get_anns_path(self, read_mode):
+        return osp.join(self.db_path, "{}_{}_anns.pkl".format(self.dataType, str(read_mode)))
+
+    def get_unique_ids(self, mask, return_counts=False, exclude_ids=[0, 255]):
+        ids, sizes = np.unique(mask, return_counts=True)
+        ids = list(ids)
+        sizes = list(sizes)
+        for ex_id in exclude_ids:
+            if ex_id in ids:
+                id_index = ids.index(ex_id)
+                ids.remove(ex_id)
+                sizes.remove(sizes[id_index])
+
+        assert (len(ids) == len(sizes))
+
+        if return_counts:
+            return ids, sizes
+        else:
+            return ids
+
+    def create_anns(self, read_mode):
+        list_dir = 'metadata/pascalvoc12/train_aug_id.txt'
+        if self.dataType == "val":
+            list_dir = 'metadata/pascalvoc12/val_id.txt'
+        with open(list_dir, 'r') as f:
+            lines = f.readlines()
+            names = []
+            for line in lines:
+                if line.endswith('\n'):
+                    line = line[:-1]
+                if len(line) > 0:
+                    names.append(line)
+        anns = []
+        for item in names:
+            mclass_path = osp.join(self.db_path, 'SegmentationClassAug', item + '.png')
+            mclass_uint = cv2.imread(mclass_path, cv2.IMREAD_GRAYSCALE)
+            class_ids = self.get_unique_ids(mclass_uint)
+
+            if read_mode == PASCAL_READ_MODES.SEMANTIC:
+                for class_id in class_ids:
+                    assert (class_id != 0 or class_id != 255)  # 0 is background,255 is ignore
+                    anns.append(dict(image_name=item, mask_name=item, class_ids=[class_id]))
+            elif read_mode == PASCAL_READ_MODES.SEMANTIC_ALL:
+                anns.append(dict(image_name=item, mask_name=item, class_ids=class_ids))
+            else:
+                raise ValueError
+        with open(self.get_anns_path(read_mode), 'w') as f:
+            pickle.dump(anns, f)
+
+    def load_anns(self, read_mode):
+        path = self.get_anns_path(read_mode)
+        if not osp.exists(path):
+            self.create_anns(read_mode)
+        with open(path, 'rb') as f:
+            anns = pickle.load(f)
+        return anns
+
+    def get_anns(self, catIds=[], areaRng=[], read_mode=PASCAL_READ_MODES.INSTANCE):
+        if areaRng == []:
+            areaRng = [0, np.inf]
+        anns = self.load_anns(read_mode)
+        if catIds == [] and areaRng == [0, np.inf]:
+            return anns
+
+        if read_mode == PASCAL_READ_MODES.INSTANCE:
+            filtered_anns = [ann for ann in anns if
+                             ann['class_ids'][0] in catIds and areaRng[0] < ann['object_sizes'][0] and
+                             ann['object_sizes'][0] < areaRng[1]]
+        else:
+            filtered_anns = []
+            catIds_set = set(catIds)
+            for ann in anns:
+                class_inter = set(ann['class_ids']) & catIds_set
+                # remove class_ids that we did not asked for (i.e. are not catIds_set)
+                if len(class_inter) > 0:
+                    ann = ann.copy()
+                    ann['class_ids'] = sorted(list(class_inter))
+                    filtered_anns.append(ann)
+        return filtered_anns
+
+    def getItems(self, cats=[], areaRng=[], read_mode=PASCAL_READ_MODES.INSTANCE):
+        if len(cats) == 0:
+            catIds = self.id_name_map.keys()
+        else:
+            catIds = self.getCatIds(catNms=cats)
+        catIds = np.sort(catIds)
+
+        anns = self.get_anns(catIds=catIds, areaRng=areaRng, read_mode=read_mode)
+        cprint(str(len(anns)) + ' annotations read from pascal', bcolors.OKGREEN)
+
+        items = []
+
+        ids_map = None
+        if read_mode == PASCAL_READ_MODES.SEMANTIC_ALL:
+            old_ids = catIds
+            new_ids = range(1, len(catIds) + 1)
+            ids_map = dict(zip(old_ids, new_ids))
+        for i in range(len(anns)):
+            ann = anns[i]
+            img_path = osp.join(self.db_path, 'JPEGImages', ann['image_name'] + '.jpg')
+            mask_path = osp.join(self.db_path, 'SegmentationClassAug', ann['mask_name'] + '.png')
+            item = DBPascalItem('pascal-' + self.dataType + '_' + ann['image_name'] + '_' + str(i), img_path, mask_path,
+                                ann['class_ids'], ids_map)
+            items.append(item)
+        return items
+
+    @staticmethod
+    def cluster_items(items):
+        clusters = {}
+        for i, item in enumerate(items):
+            assert (isinstance(item, DBPascalItem))
+            item_id = item.obj_ids
+            assert (len(item_id) == 1), 'For proper clustering, items should only have one id'
+            item_id = item_id[0]
+            if clusters.has_key(item_id):
+                clusters[item_id].append(item)
+            else:
+                clusters[item_id] = DBImageSetItem('set class id = ' + str(item_id), [item])
+        return clusters
 
 
 class DBInterface():
