9f3487dec6f2745bda9b6f8c6403b6dd75fad5fc
Tao Hu <taohu620@gmail.com>
2018-11-03 21:04:59 +0100

    u

--- example/incremental-seg/v1/model_handinhand.py
+++ example/incremental-seg/v1/model_handinhand.py
@@ -517,6 +517,11 @@ class HandInHandModel(nn.Module):
             b2 = self.layer2(b2)
             b2 = self.layer3(b2)
             b2 = self.layer4(b2) + b1_layer4*annealing_value
+        elif self.netstyle == 8:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2)
+            b2 = self.layer3(b2) + b1_layer3*annealing_value
+            b2 = self.layer4(b2)
         else:
             raise
 
@@ -524,8 +529,294 @@ class HandInHandModel(nn.Module):
 
         return b1, b2
 
+
+
+class HandInHandModel_Hourglass(nn.Module):
+
+    def __init__(self, block, layers, teacher_class_num, student_class_num, hourglass_depth=3, annealing = False,get_anneal=None, netstyle=0):
+        super(HandInHandModel_Hourglass, self).__init__()
+        #teacher network
+        self.teacher = MyResNet(block, layers, teacher_class_num)
+
+        self.iter = 1.0
+        self.annealing = annealing
+        self.netstyle = netstyle
+
+
+        self.get_anneal = get_anneal
+
+        self.semodule1 = ResidualAttentionModule(in_channels=64*4, size1=(119,119), size2=(60, 60), size3=(30, 30), size4=None, depth=hourglass_depth)
+        self.semodule2 = ResidualAttentionModule(in_channels=128*4, size1=(60, 60), size2=(30, 30), size3=(15, 15), size4=None, depth=hourglass_depth)
+        self.semodule3 = ResidualAttentionModule(in_channels=256*4, size1=(60, 60), size2=(30, 30), size3=(15, 15), size4=None, depth=hourglass_depth)
+        self.semodule4 = ResidualAttentionModule(in_channels=512*4, size1=(60, 60), size2=(30, 30), size3=(15, 15), size4=None, depth=hourglass_depth)
+
+
+        #student network
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64, affine = affine_par)
+        for i in self.bn1.parameters():
+            i.requires_grad = False
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1, ceil_mode=True) # change
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=1, dilation=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=1, dilation=4)
+        self.layer5 = self._make_pred_layer(Classifier_Module, [6,12,18,24],[6,12,18,24],student_class_num)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, 0.01)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+        #        for i in m.parameters():
+        #            i.requires_grad = False
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation == 2 or dilation == 4:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion,affine = affine_par))
+        for i in downsample._modules['1'].parameters():
+            i.requires_grad = False
+        layers = []
+        layers.append(block(self.inplanes, planes, stride,dilation=dilation, downsample=downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation))
+
+        return nn.Sequential(*layers)
+    def _make_pred_layer(self,block, dilation_series, padding_series,num_classes):
+        return block(dilation_series,padding_series,num_classes)
+
+
+
+
+    def forward(self, x):
+
+        b1_layer1, b1_layer2, b1_layer3, b1_layer4, b1 = self.teacher(x)
+
+        b2 = self.conv1(x)
+        b2 = self.bn1(b2)
+        b2 = self.relu(b2)
+        b2 = self.maxpool(b2)
+
+        if self.annealing:
+            annealing_value = self.get_anneal(self.iter)
+        else:
+            annealing_value = 1
+
+        if self.training:
+            #print "update!"
+            self.iter += 1
+        #print "anneal: {}".format(annealing_value)
+        if self.netstyle == 0:
+            b2 = self.layer1(b2) + self.semodule1(b1_layer1)*annealing_value
+            b2 = self.layer2(b2) + self.semodule2(b1_layer2)*annealing_value
+            b2 = self.layer3(b2) + self.semodule3(b1_layer3)*annealing_value
+            b2 = self.layer4(b2) + self.semodule4(b1_layer4)*annealing_value
+
+        elif self.netstyle == 1:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2)
+            b2 = self.layer3(b2)
+            b2 = self.layer4(b2) + self.semodule4(b1_layer4) * annealing_value
+        elif self.netstyle == 2:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2)
+            b2 = self.layer3(b2)+ self.semodule3(b1_layer3)*annealing_value
+            b2 = self.layer4(b2) + self.semodule4(b1_layer4) * annealing_value
+        elif self.netstyle == 3:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2) + self.semodule2(b1_layer2)*annealing_value
+            b2 = self.layer3(b2)+ self.semodule3(b1_layer3)*annealing_value
+            b2 = self.layer4(b2) + self.semodule4(b1_layer4) * annealing_value
+        elif self.netstyle == 4:
+            b2 = self.layer1(b2) + b1_layer1*annealing_value
+            b2 = self.layer2(b2) + b1_layer2*annealing_value
+            b2 = self.layer3(b2) + b1_layer3*annealing_value
+            b2 = self.layer4(b2) + b1_layer4*annealing_value
+        elif self.netstyle == 5:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2) + b1_layer2*annealing_value
+            b2 = self.layer3(b2) + b1_layer3*annealing_value
+            b2 = self.layer4(b2) + b1_layer4*annealing_value
+        elif self.netstyle == 6:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2)
+            b2 = self.layer3(b2) + b1_layer3*annealing_value
+            b2 = self.layer4(b2) + b1_layer4*annealing_value
+        elif self.netstyle == 7:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2)
+            b2 = self.layer3(b2)
+            b2 = self.layer4(b2) + b1_layer4*annealing_value
+        elif self.netstyle == 8:
+            b2 = self.layer1(b2)
+            b2 = self.layer2(b2)
+            b2 = self.layer3(b2) + b1_layer3*annealing_value
+            b2 = self.layer4(b2)
+        else:
+            raise
+
+        b2 = self.layer5(b2)
+
+        return b1, b2
+
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.stride = stride
+        self.bn1 = nn.BatchNorm2d(input_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(input_channels, output_channels / 4, 1, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(output_channels / 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(output_channels / 4, output_channels / 4, 3, stride, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(output_channels / 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(output_channels / 4, output_channels, 1, 1, bias=False)
+        self.conv4 = nn.Conv2d(input_channels, output_channels, 1, stride, bias=False)
+
+    def forward(self, x):
+        residual = x
+        out = self.bn1(x)
+        out1 = self.relu(out)
+        out = self.conv1(out1)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        if (self.input_channels != self.output_channels) or (self.stride != 1):
+            residual = self.conv4(out1)
+        out += residual
+        return out
+
+
+class ResidualAttentionModule(nn.Module):
+    # input size is 112*112
+    def __init__(self, in_channels, size1=(112, 112), size2=(56, 56), size3=(28, 28), size4=(14, 14), depth=3):
+        super(ResidualAttentionModule, self).__init__()
+        out_channels = in_channels
+
+
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+        self.depth = depth
+
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # 56*56
+        self.softmax1_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.skip1_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # 28*28
+        self.softmax2_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.skip2_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # 14*14
+        self.softmax3_blocks = ResidualBlock(in_channels, out_channels)
+        self.skip3_connection_residual_block = ResidualBlock(in_channels, out_channels)
+        self.mpool4 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        # 7*7
+        self.softmax4_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+        self.interpolation4 = nn.UpsamplingBilinear2d(size=size4)
+        self.softmax5_blocks = ResidualBlock(in_channels, out_channels)
+        self.interpolation3 = nn.UpsamplingBilinear2d(size=size3)
+        self.softmax6_blocks = ResidualBlock(in_channels, out_channels)
+        self.interpolation2 = nn.UpsamplingBilinear2d(size=size2)
+        self.softmax7_blocks = ResidualBlock(in_channels, out_channels)
+        self.interpolation1 = nn.UpsamplingBilinear2d(size=size1)
+
+        self.softmax8_blocks = nn.Sequential(
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias = False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels , kernel_size=1, stride=1, bias = False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        # 112*112
+        out_trunk = x
+
+        out_mpool1 = self.mpool1(x)
+        # 56*56
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+
+        out_skip1_connection = self.skip1_connection_residual_block(out_softmax1)
+        out_mpool2 = self.mpool2(out_softmax1)
+        # 28*28
+        out_softmax2 = self.softmax2_blocks(out_mpool2)
+
+        out_skip2_connection = self.skip2_connection_residual_block(out_softmax2)
+        out_mpool3 = self.mpool3(out_softmax2)
+        # 14*14
+        out_softmax3 = self.softmax3_blocks(out_mpool3)
+
+        """
+        out_skip3_connection = self.skip3_connection_residual_block(out_softmax3)
+        out_mpool4 = self.mpool4(out_softmax3)
+        # 7*7
+        out_softmax4 = self.softmax4_blocks(out_mpool4)
+        out_interp4 = self.interpolation4(out_softmax4) + out_softmax3
+        out = out_interp4 + out_skip3_connection
+        out_softmax5 = self.softmax5_blocks(out)
+
+        """
+        out_interp3 = self.interpolation3(out_softmax3) + out_softmax2 #change out_softmax5 to out_softmax3
+        out = out_interp3 + out_skip2_connection
+        out_softmax6 = self.softmax6_blocks(out)
+
+        out_interp2 = self.interpolation2(out_softmax6) + out_softmax1
+        out = out_interp2 + out_skip1_connection
+        out_softmax7 = self.softmax7_blocks(out)
+
+        out_interp1 = self.interpolation1(out_softmax7) + out_trunk
+        out_softmax8 = self.softmax8_blocks(out_interp1)#normalize to 1
+
+        #tail part
+        out = (1 + out_softmax8) * out_trunk
+        out_last = self.last_blocks(out)
+
+        return out_last
+
+
 def get_handinhand(teacher_class_num, student_class_num, annealing = False,get_anneal=None, netstyle=0):
      model = HandInHandModel(block=Bottleneck, layers = [3, 4, 6, 3], teacher_class_num=teacher_class_num, student_class_num=student_class_num, annealing=annealing,get_anneal=get_anneal, netstyle=netstyle)
-
      return model
+
+
+def get_handinhand_hourglass(teacher_class_num, student_class_num, annealing=False, get_anneal=None, netstyle=0):
+    model = HandInHandModel_Hourglass(block=Bottleneck, layers=[3, 4, 6, 3], teacher_class_num=teacher_class_num,
+                            student_class_num=student_class_num, annealing=annealing, get_anneal=get_anneal,
+                            netstyle=netstyle)
+
+    return model
 
