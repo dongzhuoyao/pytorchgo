2aeb2b039fabd31eda7e55a249707c86969e5017
Tao Hu <taohu620@gmail.com>
2018-11-06 00:37:53 +0100

    u

--- example/incremental-seg/datalist_nonoverlap/prepare_cs_synthia_for_cross_domain.py
+++ example/incremental-seg/datalist_nonoverlap/prepare_cs_synthia_for_cross_domain.py
@@ -4,14 +4,13 @@ from tqdm import tqdm
 import cv2, os, shutil
 
 
-COCO_PATH = '/home/tao/dataset/coco14/'
-image_format = os.path.join(COCO_PATH, 'train2014/{}')
-label_format = os.path.join(COCO_PATH, "val2014/{}")
+cs_base = ""
+synthia_base = ""
 
 new_label_basedir = "/home/tao/dataset/incremental_seg"
 
 
-def conduct_filter(filter_func, train_slic=[0,-1], valtest_filter_func = None, label_name ="class19+1_old"):
+def conduct_filter(filter_func, valtest_filter_func = None, label_name ="class19+1_old"):
     #old data
 
     if os.path.exists(label_name):
@@ -23,62 +22,82 @@ def conduct_filter(filter_func, train_slic=[0,-1], valtest_filter_func = None, l
     os.makedirs(os.path.join(new_label_basedir, label_name))
 
 
-    train_num = 0;val_num=0;test_num = 0
+    train_num = 0; val_num=0; test_num = 0
     useful_train = 0; useful_val = 0; useful_test = 0
-    with open(os.path.join(label_name,"coco_incremental_train.txt"),"w") as f:
-        for key,value in tqdm(_coco.imgs.items()[train_slic[0]:train_slic[1]], desc="train images for {}".format(label_name)):
-            #if train_num > 100: break
-            image_id = value['id']
-            img_name = value['file_name']
-            _, label_image = generate_mask(_coco, image_id)
-            if filter_func is not None:
-                is_needed, label_image = filter_func(label_image)
-            #if not is_needed:
-            #    continue
-                if is_needed:
-                    useful_train += 1
-            cur_label_path = "{}".format(os.path.join(label_name, img_name)).replace("jpg", "png")
-            cv2.imwrite(os.path.join(new_label_basedir, cur_label_path), label_image)
-            f.write("{} {}\n".format(img_name, cur_label_path))
-            train_num += 1
+    with open(os.path.join(label_name,"current_incremental_train.txt"),"w") as f:
+        for data_type, img_base in zip(["cs","synthia"], [cs_base, synthia_base]):
+            lines = open(data_type,"r").readlines()
+            lines = [line.strip() for line in lines]
 
-    with open(os.path.join(label_name,"coco_incremental_val.txt"),"w") as f:
-        for key,value in tqdm(_coco_val.imgs.items()[:582], desc="val images for {}".format(label_name)):
-            #if val_num > 100: break
-            image_id = value['id']
-            img_name = value['file_name']
-            _, label_image = generate_mask(_coco_val, image_id)
-            if valtest_filter_func is not None:
-                is_needed, label_image = valtest_filter_func(label_image)
-            #if not is_needed:
-            #    continue
-                if is_needed:
-                    useful_val += 1
+            for line  in tqdm(lines[0:1000], desc="train images for {}/{}".format(label_name,data_type)):
+                #if train_num > 100: break
 
-            cur_label_path = "{}".format(os.path.join(label_name, img_name)).replace("jpg", "png")
-            cv2.imwrite(os.path.join(new_label_basedir, cur_label_path), label_image)
-            f.write("{} {}\n".format(img_name, cur_label_path))
-            val_num += 1
+                image, label = line.split()
+                img_name = image.split("/")[-1]
+
+                label_image = cv2.imread(os.path.join(img_base, label))
+                if filter_func is not None:
+                    is_needed, label_image = filter_func(label_image)
+                #if not is_needed:
+                #    continue
+                    if is_needed:
+                        useful_train += 1
+                cur_label_path = "{}".format(os.path.join(label_name, img_name)).replace("jpg", "png")
+                cv2.imwrite(os.path.join(new_label_basedir, cur_label_path), label_image)
+                f.write("{} {}\n".format(img_name, cur_label_path))
+                train_num += 1
 
 
-    with open(os.path.join(label_name, "coco_incremental_test.txt"),"w") as f:
-        for key,value in tqdm(_coco_val.imgs.items()[582:582+1449], desc="test images for {}".format(label_name)):
-            #if val_num > 100: break
-            image_id = value['id']
-            img_name = value['file_name']
-            _, label_image = generate_mask(_coco_val, image_id)
-            if valtest_filter_func is not None:
-                is_needed, label_image = valtest_filter_func(label_image)
 
-            #if not is_needed:
-            #    continue
-                if is_needed:
-                    useful_test += 1
+    with open(os.path.join(label_name,"current_incremental_val.txt"),"w") as f:
+        for data_type, img_base in zip(["cs","synthia"], [cs_base, synthia_base]):
+            lines = open(data_type,"r").readlines()
+            lines = [line.strip() for line in lines]
 
-            cur_label_path = "{}".format(os.path.join(label_name, img_name)).replace("jpg", "png")
-            cv2.imwrite(os.path.join(new_label_basedir, cur_label_path), label_image)
-            f.write("{} {}\n".format(img_name, cur_label_path))
-            test_num += 1
+            for line  in tqdm(lines[0:500], desc="val images for {}/{}".format(label_name,data_type)):
+                #if train_num > 100: break
+
+                image, label = line.split()
+                img_name = image.split("/")[-1]
+
+                label_image = cv2.imread(os.path.join(img_base, label))
+                if valtest_filter_func is not None:
+                    is_needed, label_image = valtest_filter_func(label_image)
+                #if not is_needed:
+                #    continue
+                    if is_needed:
+                        useful_val += 1
+
+                cur_label_path = "{}".format(os.path.join(label_name, img_name)).replace("jpg", "png")
+                cv2.imwrite(os.path.join(new_label_basedir, cur_label_path), label_image)
+                f.write("{} {}\n".format(img_name, cur_label_path))
+                val_num += 1
+
+
+    with open(os.path.join(label_name, "current_incremental_test.txt"),"w") as f:
+        for data_type, img_base in zip(["cs","synthia"], [cs_base, synthia_base]):
+            lines = open(data_type,"r").readlines()
+            lines = [line.strip() for line in lines]
+
+            for line  in tqdm(lines[0:500], desc="test images for {}/{}".format(label_name,data_type)):
+                #if train_num > 100: break
+
+                image, label = line.split()
+                img_name = image.split("/")[-1]
+
+                label_image = cv2.imread(os.path.join(img_base, label))
+                if valtest_filter_func is not None:
+                    is_needed, label_image = valtest_filter_func(label_image)
+
+                #if not is_needed:
+                #    continue
+                    if is_needed:
+                        useful_test += 1
+
+                cur_label_path = "{}".format(os.path.join(label_name, img_name)).replace("jpg", "png")
+                cv2.imwrite(os.path.join(new_label_basedir, cur_label_path), label_image)
+                f.write("{} {}\n".format(img_name, cur_label_path))
+                test_num += 1
 
     print "train num={}, useful num={}".format(train_num, useful_train)
     print "val num={}, useful num={}".format(val_num, useful_val)
